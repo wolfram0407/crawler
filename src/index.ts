29ef29f399b69cf373ec6b7d23e47ffbf5ae73dd
WreckerKim <wreckerbm@gmail.com>
2024-09-24 10:09:10 +0900

    로그인 함수로 분리 evaluate 추가

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,25 @@ const initBrowser = async () => {
   return browser;
 }
 
+const login = async (page : puppeteer.Page) => {
+  await page.goto("https://www.saucedemo.com");
+  await page.type("#user-name", "standard_user");
+  await page.type("#password", "secret_sauce");
+  await page.click("#login-button");
+}
 (async () => {
   const browser = await initBrowser();
   const page = await browser.newPage();
 
-  await page.goto("https://www.saucedemo.com");
-  // select # 필요
-  await page.type("#user-name", "standard_user");
-  await page.type("#password", "secret_sauce");
+  await login(page);
 
-  await page.click("#login-button");
-
-
+  const productNames = page.evaluate(() => {
+    let productName: string[] = [];
+    const elements = document.querySelectorAll(".inventory_item_name");
+    // elements map method 동작하지 않음
+    elements.forEach((element) =>productName.push(element.innerHTML));
+    return productName;
+  })
+  console.log(productNames);
   //await browser.close();
 })();
